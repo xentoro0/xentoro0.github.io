f51c4a5250d73a87ea73bd67d090ab374aa51f00
dior hyseni <xentoro00@gmial.com>
2021-05-09 01:57:41 +0200

    changess

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const main = document.getElementById("main-div");
 
-for(let i = 0;i < 10;i++){
-    main.innerHTML += "<div></div> \n";
+
+var jsonD ;
+const options = {
+    method: "GET",
+    mode: 'cors', 
+    cache: 'no-cache',
+    credentials: 'same-origin', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+}
+fetch('http://localhost:5501/api')
+    .then(res => res.json())
+    .then(json => {
+        jsonRes(json)
+        console.log((json))
+    });
+function jsonRes(data){
+    for(let i = 0;i < 10;i++){
+        main.innerHTML += `<div class='clipsDiv'>
+                            <iframe class="embedly-embed" src="${data[i].link}" width="600" height="340" scrolling="no" title="Twitch.tv embed" frameborder="0" ></iframe>
+                            <h1 class="clipsText">${data[i].title}</h1>
+                            </div> \n`;
+    }
 }
