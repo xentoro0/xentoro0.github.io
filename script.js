635c05e66b944fc7be7f13f9264af862c96378cb
dior hyseni <xentoro00@gmial.com>
2021-05-11 23:27:32 +0200

    updated

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 const main = document.getElementById("main-div");
-
-
-var jsonD ;
-const options = {
-    method: "GET",
-    mode: 'cors', 
-    cache: 'no-cache',
-    credentials: 'same-origin', 
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow', 
-    referrerPolicy: 'no-referrer', 
-}
-fetch('http://localhost:5501/api')
-    .then(res => res.json())
-    .then(json => {
-        jsonRes(json)
-        console.log((json))
-    });
-function jsonRes(data){
-    for(let i = 0;i < 10;i++){
-        main.innerHTML += `<div class='clipsDiv'>
-                            <iframe class="embedly-embed" src="${data[i].link}" width="600" height="340" scrolling="no" title="Twitch.tv embed" frameborder="0" ></iframe>
-                            <h1 class="clipsText">${data[i].title}</h1>
-                            </div> \n`;
+const options = document.getElementById("options");
+const Time = document.getElementById("Time");
+var errors = 0;
+sendJson();
+function change() {
+    if(options.value == 'top'){
+        Time.style.display = 'inline';
+    }else{
+        Time.style.display = 'none';
     }
 }
+window.onload = e => {
+    for(let i = 0;i < 10;i++){
+        jsonRes(" ", "Title", 0, '"Author"')
+    }
+}
+function jsonRes(link, title, ups, author){
+        main.innerHTML +=  `<div class='clipsDiv'>
+                                <iframe class="clipsFrame" src="${link}" width="600" height="340" scrolling="no" title="Twitch.tv embed" frameborder="0" ></iframe>
+                                <p><span>${ups}</span> upvotes</p><p class="author">Posted by user: <span>${author}</span></p>
+                                <h1 class="clipsText">${title}</h1>
+                            </div> \n`;
+}
+
+function fetchData() {
+    fetch('http://localhost:5501/api')
+        .then(res => res.json())
+        .then( json => {
+            main.innerHTML = '';
+            for(let i = 0;i < 10;i++){
+                jsonRes(json.body[i].link, json.body[i].title, json.body[i].upvotes, json.body[i].author)
+            }
+            console.log(json)
+        })
+        .catch(err => {
+            errors++;
+            if(errors < 50){
+                fetchData();
+                console.log(errors)
+            }else{
+                alert("Servers currently are offline! \nPlease try again later!");
+            }
+        })
+}
+
+function sendJson() {
+    let data = {
+        type: options.value,
+        time: Time.value
+    }
+    console.log(data);
+    fetch('http://localhost:5501/sendJson', {method:"POST", body: JSON.stringify(data), headers: {'Content-type': 'application/json'}})
+        .then(fetchData())
+    
+}
+
